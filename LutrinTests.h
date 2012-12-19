27513f52ae8b257e8853798250e20e4f4196e8bf
Shigeru Hagiwara <hgwrsgr@gmail.com>
2012-12-20 05:29:54 +0900

    add unit tests

---
+++ LutrinTests.h
@@ -0,0 +1,17 @@
+//
+//  LutrinTests.h
+//  lutrin
+//
+//  Created by Shigeru Hagiwara on 12/12/20.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface LutrinTests : SenTestCase {
+
+}
+
+@end
---
+++ LutrinTests.m
@@ -0,0 +1,29 @@
+//
+//  LutrinTests.m
+//  lutrin
+//
+//  Created by Shigeru Hagiwara on 12/12/20.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "LutrinTests.h"
+
+
+@implementation LutrinTests
+
+- (void)setUp
+{
+	[super setUp];
+}
+
+- (void)tearDown
+{
+	[super tearDown];
+}
+
+- (void)testExample
+{
+	// STFail(@"fail");
+}
+
+@end
