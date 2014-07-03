861c3cee8655810d063eba0c7b0b181377d917ce
Shigeru Hagiwara <hgwrsgr@gmail.com>
2014-07-03 17:59:58 +0900

    version 2.1

--- MyNSApplication.m
+++ MyNSApplication.m
@@ -13,20 +13,39 @@
 - (void)sendEvent:(NSEvent *) event
 {
     if ([event type] == NSKeyDown) {
-        // スペースキーのイベントを、右矢印キーイベントに置き換えます。
-        if( [[event charactersIgnoringModifiers] isEqualToString:@" "] ) {
-            const unichar right[1] = { NSRightArrowFunctionKey };
-            NSString *characters = [NSString stringWithCharacters:right length:1];
+        // キーイベントを書き換えます。
+        // スペースキー → 右矢印キー
+        // j → 右矢印キー
+        // k → 左矢印キー
+        const unichar right[1] = { NSRightArrowFunctionKey };
+        const unichar left[1] = { NSLeftArrowFunctionKey };
+        NSString *rightChars = [NSString stringWithCharacters:right length:1];
+        NSString *leftChars = [NSString stringWithCharacters:left length:1];
+
+        if( [[event charactersIgnoringModifiers] isEqualToString:@" "] ||
+            [[event charactersIgnoringModifiers] isEqualToString:@"j"] ) {
             event = [NSEvent keyEventWithType:[event type]
                                      location:[event locationInWindow]
                                 modifierFlags:[event modifierFlags]
                                     timestamp:[event timestamp]
                                  windowNumber:[event windowNumber]
                                       context:[event context]
-                                   characters:characters
-                  charactersIgnoringModifiers:characters
+                                   characters:rightChars
+                  charactersIgnoringModifiers:rightChars
                                     isARepeat:[event isARepeat]
                                       keyCode:124 // Right key
+                     ];
+        } else if( [[event charactersIgnoringModifiers] isEqualToString:@"k"] ) {
+            event = [NSEvent keyEventWithType:[event type]
+                                     location:[event locationInWindow]
+                                modifierFlags:[event modifierFlags]
+                                    timestamp:[event timestamp]
+                                 windowNumber:[event windowNumber]
+                                      context:[event context]
+                                   characters:leftChars
+                  charactersIgnoringModifiers:leftChars
+                                    isARepeat:[event isARepeat]
+                                      keyCode:123 // Left key
                      ];
         }
     }
